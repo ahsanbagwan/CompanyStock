eaa7eccc9e63245b384d0182609b08cf649fcb70
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-09-30 19:14:13 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$.ajax({
+    	type : "GET",
+     	dataType : "json",
+     	url : "https://www.quandl.com/api/v3/datasets/NSE/ZENSARTECH.json?auth_token=KZCPhKDFd5JZwGdUJyUY",
+     	success: function(data){
+           console.log(data.dataset.data);
+     	}
+    });	
+});
